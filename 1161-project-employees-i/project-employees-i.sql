30f3c0fa519be798d3e05cc0b419006cb1e04c96
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-04-01 22:20:01 +0530

    Time: 1296 ms (35.78%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -4,5 +4,5 @@ SELECT
     ROUND(AVG(e.experience_years), 2) AS average_years
 FROM
     Project p 
-    INNER JOIN Employee e ON e.employee_id = p.employee_id
+    JOIN Employee e ON e.employee_id = p.employee_id
 GROUP BY p.project_id;
