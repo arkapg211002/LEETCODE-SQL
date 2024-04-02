7290716adf17cda57694885ec598183b5841bdfa
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-04-02 22:08:58 +0530

    Time: 1298 ms (62.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,7 @@
+WITH temp AS (SELECT ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS row_num,
+customer_id, order_date, customer_pref_delivery_date
+ FROM Delivery)
+
+SELECT ROUND(CAST(SUM(CASE WHEN row_num=1 AND order_date=customer_pref_delivery_date THEN 1 ELSE 0 END) AS BINARY_DOUBLE) / SUM(CASE WHEN row_num=1 THEN 1 ELSE 0 END) * 100, 2) AS immediate_percentage FROM temp
+
+
