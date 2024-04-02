129b99f1bc4ba56b0fac7d87555a910753b6329c
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-04-02 08:12:19 +0530

    Time: 1095 ms (70.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -0,0 +1,13 @@
+/* Write your PL/SQL query statement below */
+
+
+SELECT 
+employee_id AS "employee_id",
+(
+    CASE  
+    WHEN MOD(employee_id, 2) = 1 AND name NOT LIKE 'M%' THEN salary
+    ELSE 0 END
+) AS "bonus"
+FROM
+employees
+ORDER BY employee_id;
